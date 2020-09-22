e5b300b9f69dadc356a375170d296ed90ce40a61
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2020-09-21 23:35:23 -0400

    temporarily working
    Signed-off-by: Shiv Trivedi <shiv.v.trivedi@gmail.com>

--- commands/stop.js
+++ commands/stop.js
@@ -8,6 +8,11 @@ module.exports = {
     execute(message, args, {Canvas: Canvas, Discord: Discord}) {
         if (global.FBlistener[message.guild.id] === null) {
             message.channel.send("Not currently running.");
+        }
+        if (message.guild.voice === undefined) {
+            message.channel.send("Not currently running.");
+        } else if (!message.member.voice.channel) {
+            message.channel.send("Please join a voice channel first!");
         } else {
             try {
                 database.ref('/guilds/' + message.guild.id + '/voteState').off('value', global.FBlistener[message.guild.id]);
@@ -15,12 +20,6 @@ module.exports = {
             } catch (error) {
                 message.channel.send("Error: " + error);
             }
-        }
-        if (message.guild.voice === undefined) {
-            message.channel.send("Not currently running.");
-        } else if (!message.member.voice.channel) {
-            message.channel.send("Please join a voice channel first!");
-        } else {
             let members = message.guild.voice.channel.members;
             members.each(user => user.voice.setMute(false));
             try {
