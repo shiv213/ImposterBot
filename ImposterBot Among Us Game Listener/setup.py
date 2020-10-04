b0977afd941564d0566dd9e957f99d5b2d91debb
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2020-10-04 13:27:04 -0400

    listener

--- ImposterBot Among Us Game Listener/setup.py
+++ ImposterBot Among Us Game Listener/setup.py
@@ -1,17 +1,13 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
 setuptools.setup(
     name='ImposterBot',
     version='0.0.1',
+	long_description = "An open-source Discord bot to enhance your Among Us experience",
     packages=setuptools.find_packages(),
     url='https://github.com/shiv213/ImposterBot/',
     license='MIT',
     author='Shiv Trivedi',
     author_email='shiv.v.trivedi@gmail.com',
     description='An open-source Discord bot to enhance your Among Us experience.',
-    long_description=long_description
 )
